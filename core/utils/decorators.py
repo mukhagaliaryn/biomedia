85affa114c5ddea6811defe99b9b6807904cdfc7
mukhagaliaryn <mukhagaliaryn@gmail.com>
2025-08-25 09:47:51 +0500

    Done

--- core/utils/decorators.py
+++ core/utils/decorators.py
@@ -1,13 +1,23 @@
 from django.http import Http404
 from functools import wraps
 
+from django.shortcuts import redirect
+
 
 def role_required(*allowed_roles):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
-            if request.user.is_authenticated and request.user.user_type in allowed_roles:
-                return view_func(request, *args, **kwargs)
-            raise Http404("Бет табылмады")
+            user = request.user
+
+            if user.is_authenticated:
+                if user.user_type == 'admin':
+                    return redirect('/admin/')
+
+                if user.user_type in allowed_roles:
+                    return view_func(request, *args, **kwargs)
+
+            raise Http404('Бет табылмады')
+
         return _wrapped_view
     return decorator
